4858ab25d2981c346529c46d10513c6bdc6c7904
Rxzer <73010960+Rxzerrrr@users.noreply.github.com>
2021-09-20 17:41:51 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,18 @@
 fx_version 'cerulean'
 game 'gta5'
 
+author 'Rxzer'
 description 'qb-glizzygobler made by Rxzer'
 version '1.0.0'
 
-shared_scripts { 
-	'@qb-core/import.lua',
+shared_scripts {
+    'config.lua',
+    '@qb-core/import.lua'
 }
 
-client_script 'client/main.lua'
-server_script 'server/main.lua'
+client_scripts {
+	'client/main.lua',
+	'client/menus.lua'
+}
+
+server_script 'server/*.lua'
