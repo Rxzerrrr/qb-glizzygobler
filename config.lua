195227ebd0da7e2b744e40de6831640506c8707e
Rxzer <73010960+Rxzerrrr@users.noreply.github.com>
2021-09-20 17:41:19 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,12 +7,36 @@ label = "Shop",
     slots = 1,
     items = {
         [1] = {
-            name = "hotdog",
+            name = "hotdogbun",
             price = 0,
             amount = 100000,
             info = {},
             type = "item",
             slot = 1,
+        },
+        [2] = {
+            name = "uncookedwienie",
+            price = 0,
+            amount = 100000,
+            info = {},
+            type = "item",
+            slot = 2,
+        },
+        [3] = {
+            name = "mustard",
+            price = 0,
+            amount = 100000,
+            info = {},
+            type = "item",
+            slot = 3,
+        },
+        [4] = {
+            name = "ketchup",
+            price = 0,
+            amount = 100000,
+            info = {},
+            type = "item",
+            slot = 4,
         },
     }
 }
